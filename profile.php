f4b3cb092ea8b659e436d52ba9b464659db8e033
leegoreu <igor.bruhov2002@gmail.com>
2024-10-18 00:24:55 +0900

    Added styles for Profile page

--- profile.php
+++ profile.php
@@ -29,8 +29,8 @@ $photo_path = null; // photo is null for now
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
     <link rel="stylesheet" href="css/style.css"> 
-    <!--css/index.css"> 
-    <css/profile.css"--> 
+    <link rel="stylesheet" href="css/index.css"> 
+    <link rel="stylesheet" href="css/profile.css"> 
     <!-- Styles for profile page -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
