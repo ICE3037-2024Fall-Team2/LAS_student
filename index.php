38a53ab9d53134772277f95f4cb8bfaa261ffa4d
leegoreu <igor.bruhov2002@gmail.com>
2024-10-17 18:13:40 +0900

    Updated styles import for index page

--- index.php
+++ index.php
@@ -18,7 +18,8 @@ $username = $_SESSION['username'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lab Reservation</title>
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/style.css"> 
+    <!--css/index.css"--> 
     
     
     <!-- importing styles for icons-->
