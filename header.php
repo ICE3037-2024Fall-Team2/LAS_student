b0f86212c2290a5093d6475966c6769ef91d0ed6
웨이칭 <wyq1102@g.skku.edu>
2024-10-18 02:36:34 +0900

    [Update] improve logout

--- header.php
+++ header.php
@@ -1,20 +1,32 @@
 <div id="header">
+<a class="no-style" href="index.php">
     <div class="left">
-        <img src="img/mini-logo-color2.png" alt="Logo" class="logo-circle">
-        <span>Lab Reservation</span>
+            <img src="img/mini-logo-color2.png" alt="Logo" class="logo-circle">
+            <span>Lab Reservation</span>
     </div>
+    </a>
     <div class="right">
         <i class="fa-solid fa-user" onclick="toggleMenu()"></i>
         <div class="dropdown" id="userMenu">
             <i class="fa-solid fa-x close-dropdown" onclick="closeMenu()"></i>
-            <a href="profile.php">Account Info</a>
+            <a href="profile.php#account-info">Account Info</a>
             <a href="profile.php#reservations-info">Reservations</a>
             <a href="profile.php#past-reservations">Past Reservations</a>
             <a href="profile.php#change-password">Change Password</a>
-            <a href="login.php" class="logout">Logout</a>
+            <a href="logout.php" class="logout">Logout</a>
         </div>
     </div>
 </div>
+
+<style>
+    .no-style {
+            text-decoration: none; 
+            color: inherit; 
+        }
+    span {
+        
+    }
+</style>
 
 <script>
 function toggleMenu() {
@@ -27,12 +39,12 @@ function closeMenu() {
     menu.classList.remove('active');
 }
 
-// Close the menu if the user clicks outside of it
+// Close the dropdown if the user clicks outside of it
 document.addEventListener('click', function(event) {
     var menu = document.getElementById('userMenu');
     var icon = document.querySelector('.fa-user');
 
-    // If outside, close the menu
+    // If the click is outside the dropdown and the icon, close the dropdown
     if (!menu.contains(event.target) && !icon.contains(event.target)) {
         menu.classList.remove('active');
     }
